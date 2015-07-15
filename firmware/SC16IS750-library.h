2e6fc195c160f722ae9032f417fcd59bb07db169
jprieto155 <javierpri@gmail.com>
2015-07-15 17:19:45 +0200

    Update SC16IS750-library.h

--- firmware/SC16IS750-library.h
+++ firmware/SC16IS750-library.h
@@ -119,10 +119,10 @@ Please keep the above information when you use this code in your project.
 
 
 
-class SC16IS750 // : public Stream
+class SC16IS750-library // : public Stream
 {
     public:
-        SC16IS750(uint8_t prtcl = SC16IS750_PROTOCOL_I2C, uint8_t addr = SC16IS750_ADDRESS_AD);
+        SC16IS750-library(uint8_t prtcl = SC16IS750_PROTOCOL_I2C, uint8_t addr = SC16IS750_ADDRESS_AD);
         void begin(uint32_t baud);
         int read();
         size_t write(uint8_t val);
