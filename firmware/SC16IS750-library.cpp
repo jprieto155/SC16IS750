044d58ce6c5e100df86e009ead0a4df9ae6f4ea5
jprieto155 <javierpri@gmail.com>
2015-07-15 17:04:01 +0200

    Update SC16IS750-library.cpp

--- firmware/SC16IS750-library.cpp
+++ firmware/SC16IS750-library.cpp
@@ -21,6 +21,7 @@ Please keep the above information when you use this code in your project.
 #ifndef _SC16IS750_H_
 #define _SC16IS750_H_
 
+#include "config.h"
 #include "SC16IS750-library.h"
 #include "application.h"
 
